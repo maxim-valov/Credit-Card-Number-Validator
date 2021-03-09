dd235e16b3bbc740b70ff1e46725fd46e896db9d
Maxim Valov <max-valov@yandex.ru>
2021-03-09 20:35:10 +0300

    добавлены номера карт visa

--- Credit Card Number Validator/src/Main.java
+++ Credit Card Number Validator/src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         // TODO: подставлять номер карты нужно сюда между двойными кавычками, без пробелов
-        String number = "375833212708235";
+        String number = "4508436110470529149";
         System.out.println(String.format("Result is %s", isValidCardNumber(number) ? "OK" : "FAIL"));
     }
 
